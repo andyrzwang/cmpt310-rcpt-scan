8cfc8fc4f03862ed10427abc25514bcdbd207c86
Fustad <fapustad@yahoo.com>
2025-07-15 22:09:12 -0700

    commented code

--- src/preprocess/grayscale.py
+++ src/preprocess/grayscale.py
@@ -1,9 +1,15 @@
-
+# Importing Needed libraries
 import cv2
 
+# Creating the grayscale funciton. 
+# It will take in an input of an images file path, then 
+# covert it to a grey scale image using the cv2 library 
+# Lastly it will return the new grayscale image
 def grayscale(file_path:str):
 
+    # Converting image to grayscale
     img_gray_mode = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
 
+    # Returning the new grayscale image
     return img_gray_mode
 
--- src/preprocess/test/grayscale_test.py
+++ src/preprocess/test/grayscale_test.py
@@ -1,21 +1,29 @@
+# Importing Needed libraries
 import sys
 import os
+import cv2
+import numpy as np
 
 # Add the path to the parent folder of preprocess
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# Importing the grayscale image conversion fucntion from grayscale.py
 from grayscale import grayscale
-import cv2
-import numpy as np
 
+# Test function to varify if an image has been successfully converted into grayscale 
 def test_grayscale():
+
+    # Testing the first image for simplicity 
     path = 'cmpt310-rcpt-scan/data/dirty/large-receipt-image-dataset-SRD/1000-receipt.jpg'
     
+    # Attaining the result of the image
     result = grayscale(path)
 
+    # Checking to see if the image is grayscale or not 
     assert result is not None, "Failed to load image"
     assert len(result.shape) == 2, "Image is not in grayscale"
 
+    # Printing the result 
     print("✅ Test passed!")
 
 if __name__ == "__main__":
