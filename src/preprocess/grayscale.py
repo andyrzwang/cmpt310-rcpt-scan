fb9f9a5409c3b40807e6f42d3d582d4fbe818957
andyrzwang <andy_wang_12@sfu.ca>
2025-07-22 12:22:29 -0700

    def grayscale param to image from file path, updated tests and all passed

--- src/preprocess/grayscale.py
+++ src/preprocess/grayscale.py
@@ -5,10 +5,11 @@ import cv2
 # It will take in an input of an images file path, then 
 # covert it to a grey scale image using the cv2 library 
 # Lastly it will return the new grayscale image
-def grayscale(file_path:str):
+def grayscale(image_file):
 
     # Converting image to grayscale
-    img_gray_mode = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    # img_gray_mode = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    img_gray_mode = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
 
     # Returning the new grayscale image
     return img_gray_mode
