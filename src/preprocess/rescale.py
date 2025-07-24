7494f8fb1bccf23026b1d03242c50fd8b655587c
andyrzwang <andy_wang_12@sfu.ca>
2025-07-23 18:28:49 -0700

    fixed rescale bug

--- src/preprocess/rescale.py
+++ src/preprocess/rescale.py
@@ -1,15 +1,23 @@
 import cv2
 import os
 
+
+# # for testing only
+# import sys
+# from file_full_path import give_colored_Image_full_File_Path
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
 def rescale(image_file):
 
-    # get the height and weight of the image
-    height, width = image_file.shape
+    # get the height and width of the image
+    
+    height, width = image_file.shape[:2]
     # print(height) #testing
     # print(width) # testing
 
     # calculate the aspect ratio
-    aspect_ratio = width / height
+    aspect_ratio = height / width
     # print(aspect_ratio) #testing
 
     # set new width and new height
@@ -23,19 +31,28 @@ def rescale(image_file):
     return resized_image
 
 
-
+# testing function
 # if __name__ == "__main__":
 #     file_name = '1000-receipt.jpg'
 #     full_file = give_colored_Image_full_File_Path(file_name)
 #     print('++++++++++++++++++')
 #     print(full_file)
-#     gray_image = grayscale(full_file)
-#     scaled_image = rescale(gray_image)
+#     # gray_image = grayscale(full_file)
+#     image = cv2.imread(full_file)
+
+#     scaled_image = rescale(image)
 
 #     # testing
-#     height, width = scaled_image.shape
+#     height, width = scaled_image.shape[:2]
 #     print(height) #testing
 #     print(width) # testing  
+
+#     filesave = scaled_image
+#     output_filename = '1000-receipt-scaled.jpg'
+#     script_dir = os.path.dirname(os.path.abspath(__file__)) # current file's dir
+#     output_path = os.path.join(script_dir, output_filename)
+
+#     cv2.imwrite(output_path, filesave)
 
 #     # output_filename = 'output_image.png'
 #     # success = cv2.imwrite(output_filename, scaled_image)
