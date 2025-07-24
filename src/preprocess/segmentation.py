7cc4723814210ce780713a216341df21ceb5e530
andyrzwang <andy_wang_12@sfu.ca>
2025-07-24 16:02:14 -0700

    Everything works in stupid ways

--- src/preprocess/segmentation.py
+++ src/preprocess/segmentation.py
@@ -8,14 +8,16 @@ import sys
 
 from PIL import Image
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from file_full_path import give_colored_Image_full_File_Path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from grayscale import grayscale
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# from rescale import rescale
+from rescale import rescale
+
+from binarization import apply_binarization
+
+
 
 def rescale2(image):
     width, height = image.shape[:2]
@@ -33,20 +35,22 @@ def main(image, original):
     # gray scale
     gray_image = grayscale(image)
 
-    # # rescale +++++++++++++++++++++++++++++++
-    # scaled_image = rescale(gray_image)
+    # rescale +++++++++++++++++++++++++++++++
+    scaled_image = rescale(gray_image)
 
-    # original_scale = rescale(original)
-    # original = original_scale
-
-    # rescale2 --------------------------------
-    scaled_image = rescale2(gray_image)
-    original_scale = rescale2(original)
+    original_scale = rescale(original)
     original = original_scale
+    
+    # binarization
+    binarized_image = apply_binarization(scaled_image)
+
+    kernel = np.ones((5,5), np.uint8)
+    erosion = cv2.erode(binarized_image, kernel, iterations = 3)
+    opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
 
 
     # remove noise
-    blurred = cv2.GaussianBlur(scaled_image, (5, 5), 0)
+    blurred = cv2.GaussianBlur(opening, (5, 5), 0)
 
 
     # detect white pixels and regions
@@ -61,13 +65,14 @@ def main(image, original):
     contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # highlight the contours and store them in a new image
     image_with_contours = cv2.drawContours(original.copy(), contours, -1, (0,0,255), 3)
+    image_with_contours = cv2.drawContours(opening.copy(), contours, -1, (0,0,255), 3)     ## which should I use? original or opening?
 
 
     ## +++++++++++++++++++++++++
     # find the contour for the receipt
     # Add image border to help detect receipts touching the edge
     border_size = 10
-    bordered = cv2.copyMakeBorder(scaled_image, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=255)
+    bordered = cv2.copyMakeBorder(image_with_contours, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=255)
     bordered_blurred = cv2.GaussianBlur(bordered, (5, 5), 0)
     bordered_dilated = cv2.dilate(bordered_blurred, rectKernel)
     bordered_edged = cv2.Canny(bordered_dilated, 100, 200, apertureSize=3)
@@ -93,22 +98,27 @@ def main(image, original):
     
     image_with_receipt_contour = cv2.drawContours(original.copy(), [receipt_contour], -1, (0, 0, 255), 2)
 
-
+    print("333")
     # save file - - - - CHANGE HERE
     filesave = image_with_receipt_contour
-    output_filename = '1000-receipt-edged.jpg'
+    output_filename = '1000-receipt-binary.jpg'
     script_dir = os.path.dirname(os.path.abspath(__file__)) # current file's dir
     output_path = os.path.join(script_dir, output_filename)
 
     cv2.imwrite(output_path, filesave)
+
+
+    print("444")
     
 
 
 
 
 if __name__ == "__main__":
-    imageName = "1002-receipt.jpg"
+    imageName = "1005-receipt.jpg"
     file_name = give_colored_Image_full_File_Path(imageName)
     image = cv2.imread(file_name)
-
+    print("1")
     main(image, image)
+
+    print("Segmentation completed. Check the output file in the same directory as this script.")
