1db86e1538dd7ae9dfda1b8eb652870deee7569c
Daniel <daniel101shi@gmail.com>
2025-07-29 14:49:33 -0700

    added clahe to grayscale

--- src/preprocess/grayscale.py
+++ src/preprocess/grayscale.py
@@ -7,10 +7,12 @@ import cv2
 # Lastly it will return the new grayscale image
 def grayscale(image_file):
 
-    # Converting image to grayscale
-    # img_gray_mode = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-    img_gray_mode = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
-
-    # Returning the new grayscale image
-    return img_gray_mode
+    # grayscale the image
+    gray = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
+    
+    # create clahe object
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    # apply clahe to the grayscale image
+    enhanced = clahe.apply(gray)
+    return enhanced
 
